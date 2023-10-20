610e8d4a227917756def32d441891e2dbc381d30
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-10-20 08:24:10 -0300

    Finalizando dia 2 e iniciando di a 3

--- dia-2/relacionamentos1p1/src/services/employee.service.js
+++ dia-2/relacionamentos1p1/src/services/employee.service.js
@@ -10,21 +10,22 @@ const getAll = async () => {
   return users;
 };
 
-// const getById = async (id) => {
-//   const employee = await Employee.findOne({
-//       where: { id },
-//       // a propriedade attributes exclui o campo 'number' do retorno da requisição
-//       include: [{ model: Address, as: 'addresses', attributes: { exclude: ['number']} }],
-//     });
-//   return employee;
-// }
-
-
+// eager loading:
 const getById = async (id) => {
   const employee = await Employee.findOne({
-    where: { id },
-  });
+      where: { id },
+      // a propriedade attributes exclui o campo 'number' do retorno da requisição
+      include: [{ model: Address, as: 'addresses', attributes: { exclude: ['number']} }],
+    });
   return employee;
 }
+
+// lazy loading:
+// const getById = async (id) => {
+//   const employee = await Employee.findOne({
+//     where: { id },
+//   });
+//   return employee;
+// }
 
 module.exports = { getAll, getById };
